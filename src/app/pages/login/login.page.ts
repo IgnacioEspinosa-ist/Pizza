f20d0f5d8a4287161356852300cd82ef9b5a91bb
IgnacioEspinosa-ist <168784238+IgnacioEspinosa-ist@users.noreply.github.com>
2024-08-25 14:17:00 -0400

    Inicio2

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -14,17 +14,16 @@ export class LoginPage {
 
   login() {
     if (this.username && this.password) {
-      // Aquí iría la llamada al servicio para validar las credenciales.
-      // Pero por ahora validamos de manera fija.
+     
       if (this.username === 'admin' && this.password === '1234') {
-        // Login exitoso, redirigir a la página principal.
+        
         this.navCtrl.navigateForward('/home');
       } else {
-        // Mostrar mensaje de error
+        
         alert('Invalid credentials');
       }
     } else {
-      alert('Please enter both username and password');
+      alert('Ingrese el Nombre y Contraseña Correcta');
     }
   }
 }
