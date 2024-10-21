8c112be41c72649cc5414210a12533fa3b50c045
IgnacioEspinosa-ist <168784238+IgnacioEspinosa-ist@users.noreply.github.com>
2024-10-21 20:38:15 -0300

    registro error

--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -37,49 +37,45 @@ export class RegisterPage {
   }
 
   async register() {
-    
     this.emailError = '';
     this.rutError = '';
     this.passwordError = '';
-    this.confirmPassword ='';
-    
-
-    
+    this.passwordMismatchError = ''; // Cambiado aquí para reiniciar el mensaje
+  
+    // Validaciones del correo
     if (!this.email) {
       this.emailError = 'El correo es requerido.';
     } else if (!/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(this.email)) {
       this.emailError = 'Correo inválido.';
     }
-
-   
+  
+    // Validaciones del RUT
     if (!this.rut) {
       this.rutError = 'El RUT es requerido.';
     } else if (!/^\d{1,2}\d{3}\d{3}[-]{1}[0-9kK]{1}$/.test(this.rut)) {
       this.rutError = 'Formato de RUT inválido. Solo números y guion son permitidos.';
     }
-
-    
+  
+    // Validaciones de la contraseña
     if (!this.password) {
       this.passwordError = 'La contraseña es requerida.';
     } else if (this.password.length < 8) {
       this.passwordError = 'La contraseña debe tener al menos 8 caracteres.';
     }
-
+  
+    // Validación de coincidencia de contraseñas
     if (this.password !== this.confirmPassword) {
       this.passwordMismatchError = 'Las contraseñas no coinciden';
-      return;
+      return; // Asegúrate de retornar aquí para evitar que se registre el usuario
     }
-
-    
-    if (!this.emailError && !this.rutError && !this.passwordError) {
+  
+    // Si todas las validaciones son correctas
+    if (!this.emailError && !this.rutError && !this.passwordError && !this.passwordMismatchError) {
       console.log('Formulario válido:', { email: this.email, rut: this.rut, password: this.password });
       await this.presentAlert();
       this.router.navigate(['/login']);
-      
     } else {
       console.log('Formulario inválido');
     }
-
-    
   }
 }
