754bab8fec6f790ec582cf7b276aa6c7e9bc8db6
IgnacioEspinosa-ist <168784238+IgnacioEspinosa-ist@users.noreply.github.com>
2024-11-10 18:25:41 -0300

    asddad

--- src/app/pages/cart/cart.page.ts
+++ src/app/pages/cart/cart.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AlertController } from '@ionic/angular';
+import { CarritoService } from 'src/app/services/carrito.service';
 import { DatabaseService } from 'src/app/services/database.service';
 import { Producto } from 'src/app/services/producto';
 import { Storage } from '@ionic/storage-angular';
@@ -13,13 +14,16 @@ import { Router } from '@angular/router';
 export class CartPage implements OnInit {
   carrito: Producto[] = [];
   productos: Producto[] = [];
+  id_user: number | null = null;
+  id_direccion: number | null = null;
 
   constructor(
     private alertController: AlertController,
+    private carritoService: CarritoService,
     private dbService: DatabaseService,
     private storage: Storage,
     private router: Router
-  ) {}
+  ) { }
 
   async ngOnInit() {
     await this.storage.create();
@@ -61,5 +65,53 @@ export class CartPage implements OnInit {
 
   obtenerTotalCarrito(): number {
     return this.carrito.reduce((total, producto) => total + producto.precio, 0);
+  }
+
+
+  
+  vaciarCarrito(): void {
+    this.productos = [];
+    this.storage.set('cartItems', []);
+  }
+
+
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Compra Realizada Con Éxito',
+      buttons: ['Entendido'],
+    });
+    await alert.present();
+  }
+
+  finalizarCompra() {
+    if (this.id_user === null) {
+      console.warn('ID de usuario no disponible');
+      return;
+    }
+
+    const total = this.obtenerTotalCarrito();
+    
+    this.dbService.addPedido(total, this.id_user).subscribe({
+      next: (id_pedido: number) => {
+        this.dbService.addDetallePedido(id_pedido, this.carrito).subscribe({
+          next: () => {
+            this.presentAlert();
+            this.vaciarCarrito();
+          },
+          error: (error) => {
+            console.error('Error al agregar detalles del pedido:', error);
+          }
+        });
+      },
+      error: (error) => {
+        console.error('Error al agregar el pedido:', error);
+      }
+    });
+  }
+
+  async verDetalleProducto(producto: Producto) {
+    await this.storage.set('selectedProductId', producto.id_prod);
+    this.router.navigate(['/detalle-producto']);
   }
 }
